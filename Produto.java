674798cfedfbdc1a0a2f77072bbb9316db36565c
is4-tech <isasoares0806@gmail.com>
2026-03-26 17:13:57 -0300

    Add files via upload

---
+++ Produto.java
@@ -0,0 +1,13 @@
+public class Produto {
+    String nome;
+    double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public void exibir() {
+        System.out.println("Produto: " + nome + " | Preço: R$" + preco);
+    }
+}
